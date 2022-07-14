c948d451f68034822933fc61ab3957763054cdd5
hank <we91335@gmail.com>
2022-07-14 17:07:05 +0800

    feat: 新增抽獎API Route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::namespace('App\Http\Controllers')->group(function () {
+    Route::post('/lotteries', 'LotteryController@lottery');
 });
